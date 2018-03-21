7806d28f3be814d769a1f458eb660a7a5678f32e
jnacar <jefrey.nacar@berkeley.edu>
2018-03-21 09:38:43 -0700

    Update walking.ino

--- walking.ino
+++ walking.ino
@@ -28,7 +28,7 @@ void setup() {
 void loop() {
   if (flag == 0) {
     delay(10000);
-    for (pos = 90; pos <= 110; pos += 1) { // goes from 45 degrees to 135 degrees
+    for (pos = 90; pos <= 95; pos += 1) { // goes from 45 degrees to 135 degrees
     // in steps of 1 degree
     leftFoot.write(pos);              // tell servo to go to position in variable 'pos'
     rightFoot.write(pos);
@@ -36,7 +36,7 @@ void loop() {
     }
     delay(1000);
   
-    for (pos = 90; pos <= 135; pos += 1) { // goes from 45 degrees to 135 degrees
+    for (pos = 90; pos <= 115; pos += 1) { // goes from 45 degrees to 135 degrees
     // in steps of 1 degree
     leftThigh.write(pos);              // tell servo to go to position in variable 'pos'
     rightThigh.write(pos);
@@ -47,29 +47,29 @@ void loop() {
   }
 
   delay(1000);
-  for (pos = 90; pos <= 130; pos += 1) { // goes from 45 degrees to 135 degrees
+  for (pos = 95; pos >= 85; pos -= 1) { // goes from 45 degrees to 135 degrees
     // in steps of 1 degree
-    leftFoot.write(200 - pos);              // tell servo to go to position in variable 'pos'
-    rightFoot.write(200 - pos);
+    leftFoot.write(pos);              // tell servo to go to position in variable 'pos'
+    rightFoot.write(pos);
     delay(40);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 90; pos <= 180; pos += 1) { // goes from 45 degrees to 135 degrees
+  for (pos = 115; pos >= 65; pos -= 1) { // goes from 45 degrees to 135 degrees
     // in steps of 1 degree
-    leftThigh.write(225 - pos);              // tell servo to go to position in variable 'pos'
-    rightThigh.write(225 - pos);
+    leftThigh.write(pos);              // tell servo to go to position in variable 'pos'
+    rightThigh.write(pos);
     delay(40);                       // waits 15ms for the servo to reach the position
   }
   delay(1000);
-  for (pos = 90; pos <= 130; pos += 1) { // goes from 45 degrees to 135 degrees
+  for (pos = 85; pos <= 95; pos += 1) { // goes from 45 degrees to 135 degrees
     // in steps of 1 degree
-    leftFoot.write(pos - 20);              // tell servo to go to position in variable 'pos'
-    rightFoot.write(pos - 20);
+    leftFoot.write(pos);              // tell servo to go to position in variable 'pos'
+    rightFoot.write(pos);
     delay(40);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 90; pos <= 180; pos += 1) { // goes from 45 degrees to 135 degrees
+  for (pos = 65; pos <= 115; pos += 1) { // goes from 45 degrees to 135 degrees
     // in steps of 1 degree
-    leftThigh.write(pos - 45);              // tell servo to go to position in variable 'pos'
-    rightThigh.write(pos - 45);
+    leftThigh.write(pos);              // tell servo to go to position in variable 'pos'
+    rightThigh.write(pos);
     delay(40);                       // waits 15ms for the servo to reach the position
   }
   
